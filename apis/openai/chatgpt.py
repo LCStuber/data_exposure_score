2fc25ec08eda2a47cc2a5a3c3db0189810e7efd8
mateus <22.01082-3@maua.br>
2025-05-20 23:25:54 -0300

    feat: change parameter list

--- apis/openai/chatgpt.py
+++ apis/openai/chatgpt.py
@@ -8,30 +8,38 @@ def gerar_relatorio(api_key: str, tweets_json: str) -> str:
     Retorna o CSV como string.
     """
     openai.api_key = api_key
-    header = header = '''
+    campos_csv = '''
         Usuario,NomeProvavel,IdadeEstimada,
         GeneroEstimado,OrientacaoSexualSugestiva,RelacaoAfetivaSugerida,
         ProfissaoOcupacao,EscolaridadeIndicada,LocalizacaoProvavel,
         CidadesMencionadas,ReligiaoSugerida,PosicionamentoPolitico,
         SaudeFisicaCitacoes,SaudeMentalCitacoes,UsoDeSubstancias,
-        ExpressaoDeEmocoes,TraçosDePersonalidade,TopicosRelevantes,
-        EstiloDeVida,HobbiesEInteresses,ReferenciasAFamilia,
+        TopicosRelevantes,HobbiesEInteresses,ReferenciasAFamilia,
         ExposicaoDeRelacionamentos,PadraoDePostagem,HorariosDeAtividade,
         FrequenciaDeMidia,TipoDeMidiaCompartilhada,TipoDeLinguagem,
-        Fonte_Das_Informacoes,IDs_Posts_Relevantes
+        Fonte_Das_Informacoes,IDs_Posts_Relevantes,
+        PossuiInformacaoCPF,PossuiInformacaoRG,PossuiPassaporte,
+        PossuiTituloEleitor,NomeDaMaePresente,NomeDoPaiPresente,
+        NacionalidadeMencionada,EtniaOuRacaMencionada,EnderecoMencionado,
+        TelefoneOuEmailMencionado,PossuiInformacaoBancaria, PossuiCartaoDeEmbarque,
+        IndicacaoDeRenda,ClasseSocialInferida,PossuiPatrimonioMencionado,
+        EmpregoOuEmpresaMencionada,BeneficioSocialMencionado,
+        HistoricoFinanceiroMencionado,ScoreCreditoInferido,
+        FilhosOuDependentesMencionados,RelatoDeViolenciaOuAbuso,
+        HistoricoCriminalMencionado
     '''
+
 
     prompt = (
         f"Você é um analista de perfis de Twitter. Gere um relatório CSV completo "
-        f"com o seguinte cabeçalho e preencha uma linha com dados extraídos dos tweets.
-"
+        f"com o seguinte cabeçalho e preencha uma linha com dados extraídos dos tweets."
         f"As colunas de Fonte devem ser 'certo' quando baseadas em dados diretos "
         f"e 'inferido' quando extraídas por NLP. IDs separados por ';'.\n\n"
-        f"Cabeçalho:\n{header}\n\n"
+        f"Cabeçalho:\n{campos_csv}\n\n"
         f"Dados JSON de tweets:\n{tweets_json}"
     )
     response = openai.ChatCompletion.create(
-        model="gpt-4.1-mini",
+        model="gpt-4.1-nano",
         messages=[
             {"role": "system", "content": "Analista de perfis de Twitter"},
             {"role": "user", "content": prompt}
